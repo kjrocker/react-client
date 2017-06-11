5feefe9f637d338bb1fe8ba5399923b8ca413c89
Kevin Rocker <kevin.j.rocker@gmail.com>
2017-06-11 12:20:55 -0400

    Start adding semantic-ui

--- src/core/app.js
+++ src/core/app.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { Menu, Button } from 'semantic-ui-react'
 
 import { LoginButton } from '../auth'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { activeItem: 'home' }
+  }
+
   render() {
+    const { activeItem } = this.state
     return (
       <div>
-        <h3>Every page!</h3>
-        <LoginButton/>
+        <Menu>
+          <Menu.Menu position='right'>
+            <Menu.Item active={activeItem === 'login/logout'}>
+              <Button as={LoginButton}/>
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
         <div>
           {this.props.children}
         </div>
